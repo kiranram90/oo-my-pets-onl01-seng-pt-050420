f0e81182807e889a07ed2dbf18afb991648e7fa2
kiran ram <kram@lions.molloy.edu>
2020-06-09 03:28:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,18 @@
 class Dog
   
+  @@all = []
+  attr_reader :name 
+  attr_accessor :owner, :mood 
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end
   
 end
